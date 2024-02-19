28eba99e7724dd5b5c3943a191aada7f24632e38
BashmanivskiyMaxim <ipz203_bmo@student.ztu.edu.ua>
2024-02-19 22:33:11 +0200

    add inputs

--- src/components/form.js
+++ src/components/form.js
@@ -1,24 +1,46 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import "./form.css";
+
+// Структура брифу
+// 1. Спосіб звʼязку (як звати, телефон, терміни часу)
+// 2. Загальні відомості (назва продукту, суть, цінності, вигода клієнта)
+// 3. Цільова аудиторія
+// 4. Для чого потрібен сайт компанії (чи продукт звичайний для клієнта)
+// 5. Конкуренти
+// 6. Масштаб інтересів
+// 7. Питання оцінок вартості (структура, дизайни, модулі)
+// 8. Додаткові питання для продуктивності (просування, наповнення, що подобається, що ні)
+// 9. Додаткова інформація
 
 export default function Form() {
   const {
     register,
     handleSubmit,
-    control,
-    submissionId,
     formState: { errors, isSubmitting },
-  } = useForm({
-    defaultValues: {
-      "last-first-name": "",
-      "number-phone": "",
-      messenger: "",
-      "available-time": ["morning"],
-      "free-week": ["saturday", "sunday"],
-    },
-  });
-  const onSubmit = (data) => alert(JSON.stringify(data));
+  } = useForm();
+  const [submissionId, setSubmissionId] = useState(null);
+  const onSubmit = async (data) => {
+    try {
+      const response = await fetch("http://localhost:5000/form", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ data }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      //const responseData = await response.json();
+      //setSubmissionId(responseData._id);
+    } catch (error) {
+      console.error("There was an error!", error);
+      // Handle error state
+    }
+  };
 
   if (submissionId) {
     return <p>Thank you! Submission Id: {submissionId}</p>;
@@ -114,8 +136,103 @@ export default function Form() {
           <p role="alert">{errors["free-week"]?.message}</p>
         )}
       </div>
+      <div>
+        <span>
+          На який термін розробки проекту ви розраховуєте? (Вкажіть кінцеву дату
+          або кількість тижнів, необхідних для завершення проекту).
+        </span>
+        <input
+          {...register("project-terms", { required: "це поле обов'язкове!" })}
+          type="text"
+        />
+      </div>
+      <div>
+        <span>Назва продукту, для якого створюється веб-сайт</span>
+        <input
+          {...register("product-name", { required: "це поле обов'язкове!" })}
+          type="text"
+        />
+      </div>
+      <div>
+        <span>
+          Чи є у вас зараз сайт? Що вам подобається і не подобається у ньому?
+        </span>
+        <input
+          {...register("product-essence", { required: "це поле обов'язкове!" })}
+          type="text"
+        />
+      </div>
+      <div>
+        <span>
+          Яка кінцева мета веб-сайту? Яка його суть?(наприклад, прямі продажі,
+          надання інформації про компанію, використання як вітрини товарів)
+        </span>
+        <input
+          {...register("product-goal", { required: "це поле обов'язкове!" })}
+          type="text"
+        />
+      </div>
+      <div>
+        <span>Чи маєте ви додаткові цілі?</span>
+        <div class="tooltip">
+          <i aria-hidden="true">Підказка...</i>
 
-      <button disabled={isSubmitting}>Submit</button>
+          <span class="tooltiptext">
+            <ul>
+              <li>Покращення логічної структури сайту</li>
+              <li>Залучення більшої кількості клієнтів</li>
+              <li>Впровадження сучасних трендів дизайну</li>
+              <li>Створення багатомовного сайту</li>
+              <li>Збільшення впізнаваності бренду</li>
+              <li>Просування нового продукту або послуги</li>
+              <li>Збільшення кількості продажів</li>
+              <li>Інформування (інформаційна мета)</li>
+              <li>Ребрендинг або редизайн попереднього сайту</li>
+              <li>SEO-оптимізація сайту, усунення помилок</li>
+            </ul>
+          </span>
+        </div>
+        <textarea
+          {...register("adtional-goal", { required: "це поле обов'язкове!" })}
+          type="text"
+        />
+      </div>
+      <div>
+        <span>
+          Вкажіть особливості бізнес-ніші, в якій ви працюєте. Що визначає
+          конкуренцію в ніші?
+        </span>
+        {[
+          { label: "Технологічний прогрес", value: "Технологічний прогрес" },
+          { label: "Бренд і репутація", value: "Бренд і репутація" },
+          {
+            label: "Якість продукту / обслуговування",
+            value: "Якість продукту / обслуговування",
+          },
+          { label: "Маркетингові стратегії", value: "Маркетингові стратегії" },
+          { label: "Регулююче середовище", value: "Регулююче середовище" },
+        ].map(({ label, value }, index) => {
+          return (
+            <label key={value + index}>
+              <span>{label}</span>
+              <input
+                {...register("business-niche-optional", {
+                  required: true,
+                })}
+                aria-invalid={errors["free-week"] ? "true" : "false"}
+                value={value}
+                type="checkbox"
+              />
+            </label>
+          );
+        })}
+        <input
+          {...register("business-niche", { required: "це поле обов'язкове!" })}
+          type="text"
+        />
+      </div>
+
+      <button disabled={isSubmitting}>Відправити</button>
     </form>
   );
 }
